3ba1136adb0807ed8f9369d5d65848803a84de3a
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2026-02-20 15:11:30 +0530

    Update DFS.py

--- DFS Shortest Path/DFS.py
+++ DFS Shortest Path/DFS.py
@@ -1,7 +1,5 @@
 # Author: Jigesh Sheoran
-# SAP ID: 590025428
-# Experiment: DFS for Shortest Path
-# Course: M.Sc. Cyber Security – Semester 1
+# Last Modified : 20/02/2026
 
 from collections import defaultdict
 
@@ -21,4 +19,31 @@ def dfs_shortest_path(graph, current, destination, visited, path, shortest):
     path.pop()
     visited.remove(current)
 
+# user input 
+graph = defaultdict(list)
+
+vertices = input("Enter vertices separated by space: ").split()
+
+e = int(input("Enter number of edges: "))
+print("Enter edges (u v):")
+
+for i in range(e):
+    u, v = input(f"Edge {i+1}: ").split()
+    graph[u].append(v)
+    graph[v].append(u)   # Undirected graph
+
+source = input("Enter source vertex: ")
+destination = input("Enter destination vertex: ")
+
+shortest = [None]
+dfs_shortest_path(graph, source, destination, set(), [], shortest)
+
+if shortest[0]:
+    print("\nShortest Path using DFS:")
+    print(" -> ".join(shortest[0]))
+    print("Path Length:", len(shortest[0]) - 1)
+else:
+    print("No path exists.")
+
+print("\nDFS Shortest Path executed successfully.")
 
