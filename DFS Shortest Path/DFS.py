013b89b7f8e80df0407c60e9d10d0fffcab75fe6
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2026-02-20 15:10:50 +0530

    Update DFS.py

--- DFS Shortest Path/DFS.py
+++ DFS Shortest Path/DFS.py
@@ -1,1 +1,24 @@
-def
+# Author: Jigesh Sheoran
+# SAP ID: 590025428
+# Experiment: DFS for Shortest Path
+# Course: M.Sc. Cyber Security – Semester 1
+
+from collections import defaultdict
+
+def dfs_shortest_path(graph, current, destination, visited, path, shortest):
+    
+    visited.add(current)
+    path.append(current)
+
+    if current == destination:
+        if shortest[0] is None or len(path) < len(shortest[0]):
+            shortest[0] = list(path)
+    else:
+        for neighbor in graph[current]:
+            if neighbor not in visited:
+                dfs_shortest_path(graph, neighbor, destination, visited, path, shortest)
+
+    path.pop()
+    visited.remove(current)
+
+
