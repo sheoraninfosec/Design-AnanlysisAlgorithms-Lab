6c0623bef5fb9c575004958f08dd01b8d3c72d80
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2026-03-12 22:51:10 +0530

    Update timsort.py

--- Tim Sort/timsort.py
+++ Tim Sort/timsort.py
@@ -1,1 +1,74 @@
-def
+# Author: Jigesh Sheoran
+# Last Modified : 12 / 03 / 2026
+# Experiment: Tim Sort
+
+MIN_RUN = 32
+
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        temp = arr[i]
+        j = i - 1
+
+        while j >= left and arr[j] > temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = temp
+
+
+def merge(arr, l, m, r):
+    left = arr[l:m+1]
+    right = arr[m+1:r+1]
+
+    i = j = 0
+    k = l
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+
+def tim_sort(arr):
+    n = len(arr)
+
+    for start in range(0, n, MIN_RUN):
+        end = min(start + MIN_RUN - 1, n - 1)
+        insertion_sort(arr, start, end)
+
+    size = MIN_RUN
+    while size < n:
+        for left in range(0, n, 2 * size):
+
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+
+            if mid < right:
+                merge(arr, left, mid, right)
+
+        size *= 2
+
+
+# user input
+print("Tim Sort Program")
+
+arr = list(map(int, input("Enter elements separated by space:\n").split()))
+
+tim_sort(arr)
+
+print("\nSorted Array:")
+print(arr)
